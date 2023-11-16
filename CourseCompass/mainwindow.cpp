155e7c81577fa9593c570b13ca7eaf89e9b94019
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-11-16 12:02:00 -0500

    added more GUI example

--- CourseCompass/mainwindow.cpp
+++ CourseCompass/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->CurrentCoursesPanel->setStyleSheet("background-color: #ECECEC; border-radius: 10px; margin: 10px;");
+    ui->RecommendedActionPanel->setStyleSheet("background-color: #ECECEC; border-radius: 10px; margin: 10px;");
+    ui->ProgressTrackerPanel->setStyleSheet("background-color: #ECECEC; border-radius: 10px; margin: 10px;");
+
     // Connect the button click to the custom slot
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onHelloButtonClicked);
 }
@@ -19,4 +23,5 @@ void MainWindow::onHelloButtonClicked()
 {
     qDebug() << "Hello World button Clicked!";
     ui->groupBox->setTitle("10");
+
 }
